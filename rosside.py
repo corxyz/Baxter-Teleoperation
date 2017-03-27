03e8b6186177c755a745ccfde072287528b36166
joyceeexinyiwang <xinyiw@andrew.cmu.edu>
2017-03-26 22:46:40 -0400

    Update rosside.py

--- rosside.py
+++ rosside.py
@@ -7,43 +7,43 @@ from tornado.websocket import websocket_connect
 #ROS side data request
 
 class Client(object):
-    def __init__(self, url, timeout):
-        self.url = url
-        self.timeout = timeout
-        self.ioloop = IOLoop.instance()
-        self.ws = None
-        self.connect()
-	PeriodicCallback(self.tryAgain, 20000, io_loop=self.ioloop).start()
-        self.ioloop.start()
+	def __init__(self, url, timeout):
+		self.url = url
+		self.timeout = timeout
+		self.ioloop = IOLoop.instance()
+		self.ws = None
+		self.connect()
+		PeriodicCallback(self.tryAgain, 20000, io_loop=self.ioloop).start()
+		self.ioloop.start()
 
-    @gen.coroutine
-    def connect(self):
-        print "Connecting..."
-        try:
-            #client connect to websocket server
-            self.ws = yield websocket_connect(self.url)
-        except Exception, e:
-            print "Connection error: " + str(e)
-        else:
-            print "Connected."
-            self.run()
+	@gen.coroutine
+	def connect(self):
+		print("Connecting...")
+		try:
+			#client connect to websocket server
+			self.ws = yield websocket_connect(self.url)
+		except (Exception, e):
+			print("Connection error: " + str(e))
+		else:
+			print("Connected.")
+			self.run()
 
-    @gen.coroutine
-    def run(self):
-        while True:
-            #read message on websocket server
-            msg = yield self.ws.read_message()
-            if msg is None:
-                print "Connection closed."
-                self.ws = None
-                break
+	@gen.coroutine
+	def run(self):
+		while True:
+			#read message on websocket server
+			msg = yield self.ws.read_message()
+			if msg is None:
+				print("Connection closed.")
+				self.ws = None
+				break
 
-            print msg
+			print(msg)
 
-    def tryAgain(self):
-        if self.ws is None:
-            print "Lost connection. Trying to reconnect now..."
-            self.connect()
+	def tryAgain(self):
+		if self.ws is None:
+			print("Lost connection. Trying to reconnect now...")
+			self.connect()
 
 if __name__ == "__main__":
-    client = Client("ws://localhost:8888/ws", 5)
+	client = Client("ws://localhost:8888/ws", 5)
