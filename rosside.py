e264197d3d6b51063c141250d6c9b06bd83ea160
Alex Hong <seongkwh@andrew.cmu.edu>
2017-04-03 18:31:32 -0400

    ROS

--- rosside.py
+++ rosside.py
@@ -3,12 +3,12 @@
 from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado import gen
 from tornado.websocket import websocket_connect
-import ROS
+# import ROS
 import json
-from core_tool import *
+# from core_tool import *
 
 #ROS side data request
-d;
+d = {"left":1}
 
 class Client(object):
     def __init__(self, url, timeout):
@@ -26,7 +26,8 @@ class Client(object):
         try:
             #client connect to websocket server
             self.ws = yield websocket_connect(self.url)
-        except (Exception, e):
+        except Exception as e:
+            # print()
             print("Connection error: " + str(e))
         else:
             print("Connected.")
@@ -57,24 +58,29 @@ class Client(object):
 
 
 def Run(t):
-    def moveArm(args*):
-        arm= args[0]
-        dp= args[1]
-        x= list(t.robot.FK(arm=arm))  #Current end-effector pose
-        x_trg= [x[d]+dp[d] for d in range(3)] + x[3:]  #Target
-        print 'Moving {arm}-arm to {x}'.format(arm=LRToStr(arm), x=x_trg)
-        t.robot.MoveToX(x_trg, dt=4.0, arm=arm)
+    # def moveArm(*args):
+    #     arm= args[0]
+    #     dp= args[1]
+    #     x= list(t.robot.FK(arm=arm))  #Current end-effector pose
+    #     x_trg= [x[d]+dp[d] for d in range(3)] + x[3:]  #Target
+    #     print 'Moving {arm}-arm to {x}'.format(arm=LRToStr(arm), x=x_trg)
+    #     t.robot.MoveToX(x_trg, dt=4.0, arm=arm)
+
 
     while(d):
         if(d['right-hand'] != ""):
             println("RIGHT HAND: "  + d['Right hand'])
-            moveArm("RIGHT", d['Right hand'])
+            pos = d['Right hand']
+            pos = list(map(float, pos[1:-1].split(",")))
+            # moveArm("RIGHT", d['Right hand'])
         if(d['left-hand'] != ""):
+            pos = d['Left hand']
+            pos = list(map(float, pos[1:-1].split(",")))
             println("LEFT: "  + d['Left hand'])
-            moveArm("LEFT", d['Left hand'])
+            # moveArm("LEFT", d['Left hand'])
 
 if __name__ == "__main__":
-    client = Client("ws://localhost:8888/ws", 5)
+    client = Client("ws://128.237.212.54/ws", 5)
 
 
 
