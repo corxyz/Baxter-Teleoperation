91d7278e82bed00f0e1e14fab15cbd8c319c35de
Yishan Zhou <yishanz@andrew.cmu.edu>
2017-04-09 16:33:00 -0400

    scale action

--- rosside.py
+++ rosside.py
@@ -7,11 +7,12 @@ from tornado.websocket import websocket_connect
 import json
 # from core_tool import *
 import lib.defaultpos as dp
+import lib.base_geom as bg
 
 
 #ROS side data request
-pos = dict() #position info in the most recent frame
-dispos = dict() #disposition info from the most recent frame
+pos = dict() #position info in the most recent frame of Leap
+bpos = dict() #position info in the most recent frame of Baxter
 
 class Client(object):
     def __init__(self, url, timeout):
@@ -24,9 +25,10 @@ class Client(object):
         self.ioloop.start()
         self.dl = dp.DefaultPos() #default left hand position
         self.dr = dp.DefaultPos("R") #default right hand position
-        dispos["L"] = [dl.x, dl.y, dl.z, dl.roll, dl.pitch, dl.yaw]
-        dispos["R"] = [dr.x, dr.y, dr.z, dr.roll, dr.pitch, dr.yaw]
-
+        self.bdl = dp.BaxterDefaultPos() #Baxter default left hand position
+        self.bdr = dp.BaxterDefaultPos("R") #Baxter default right hand position
+        global pos, bpos
+        pos["L"] = pos["R"] = bpos["L"] = bpos["R"] = []
 
     @gen.coroutine
     def connect(self):
@@ -40,21 +42,37 @@ class Client(object):
             print("Connected.")
             self.run()
 
-    #disposition calculation (updates list with 7 elements)
-    def calc_desposition(self, lco, rco, lang, rang):
-        global dispos
-        dispos["L"][0] = lco[0] - self.dl.x
-        dispos["L"][1] = lco[1] - self.dl.y
-        dispos["L"][2] = lco[2] - self.dl.z
-        dispos["L"][3] = lang[0] - self.dl.roll
-        dispos["L"][4] = lang[1] - self.dl.pitch
-        dispos["L"][5] - lang[2] - self.dl.yaw
-        dispos["R"][0] = rco[0] - self.dr.x
-        dispos["R"][1] = rco[1] - self.dr.y
-        dispos["R"][2] = rco[2] - self.dr.z
-        dispos["R"][3] = rang[0] - self.dr.roll
-        dispos["R"][4] = rang[1] - self.dr.pitch
-        dispos["R"][5] - rang[2] - self.dr.yaw
+    def leap_to_baxter(self):
+        global pos, bpos
+        #calculate delta_human
+        current_l = pos["L"]
+        default_l = [self.dl.x, self.dl.y, self.dl.z]
+        default_l.append(tf.transformations.quaternion_from_euler(self.dl.roll, self.dl.pitch, self.dl.yaw))
+        delta_l = bg.TransformRightInv(current_l, default_l)
+
+        current_r = pos["R"]
+        default_r = [self.dr.x, self.dr.y, self.dr.z]
+        default_r.append(tf.transformations.quaternion_from_euler(self.dr.roll, self.dr.pitch, self.dr.yaw))
+        delta_r = bg.TransformRightInv(current_r, default_r)
+
+        #scale delta_human to delta_baxter
+        bdelta_l = dp.BaxterDefaultPos.scaleToBaxter(delta_l)
+        bdelta_r = dp.BaxterDefaultPos.scaleToBaxter(delta_r)
+
+        #calculate delta_baxter
+        #not sure if the order of arguments to bg.Transform is correct
+        bdefault_l = [self.bdl.x, self.bdl.y, self.bdl.z]
+        bdefault_l.append(tf.transformations.quaternion_from_euler(self.bdl.roll, self.bdl.pitch, self.bdl.yaw))
+        baxter_l = bg.Transform(bdefault_l, bdelta_l)
+
+        bdefault_r = [self.bdr.x, self.bdr.y, self.bdr.z]
+        bdefault_r.append(tf.transformations.quaternion_from_euler(self.bdr.roll, self.bdr.pitch, self.bdr.yaw))
+        baxter_r = bg.Transform(bdefault_r, bdelta_r)
+
+        #update Baxter position to move to (pos_baxter)
+        bpos["L"] = baxter_l
+        bpos["R"] = baxter_r
+
 
     @gen.coroutine
     def run(self):
@@ -71,20 +89,24 @@ class Client(object):
             #process message
             if(msg != ""):
                 a = list(map(lambda x: x.split("^"), msg.splitlines())
-                (l, r) = (dispos{"L"}, dispos["R"]) #init
                 if (a[0][0].startswith("Left")): l = a[0]
                 else: r = a[0]
                 if (len(a) > 1 and a[1][0].startswith("Right")): r = a[1]
-                else if (len(a) > 1): l = a[1] 
+                else if (len(a) > 1): l = a[1]
                 #get absolute 3-d coordinates
                 pos["L"] = list(map(float, l[2][1:-1].split(",")))
                 pos["R"] = list(map(float, r[2][1:-1].split(",")))
                 #absolute Euler angles
-                (lang, rang) = ([l[2][4], l[2][3], l[2][5]], 
+                (lang, rang) = ([l[2][4], l[2][3], l[2][5]],
                                 [r[2][4], r[2][3], r[2][5])
-                pos["L"].append(lang)
-                pos["R"].append(rang)
-                dispos = self.calc_disposition(lpos, lang, rpos, rang)
+                #pos["L"].append(lang)
+                #pos["R"].append(rang)
+                #transform Euler angles into Quaternion
+                lquat = tf.transformations.quaternion_from_euler(lang[0], lang[1], lang[2])
+                rquat = tf.transformations.quaternion_from_euler(rang[0], rang[1], rang[2])
+                pos["L"].append(lquat)
+                pos["R"].append(rquat)
+                self.leap_to_baxter()
 
 
     def tryAgain(self):
@@ -102,13 +124,6 @@ def Run(t):
     #     x_trg= [x[d]+dp[d] for d in range(3)] + x[3:]  #Target
     #     print 'Moving {arm}-arm to {x}'.format(arm=LRToStr(arm), x=x_trg)
     #     t.robot.MoveToX(x_trg, dt=4.0, arm=arm)
-
-    #transform Euler angles into Quaternion
-    lpos = pos["L"]
-    rpos = pos["R"]
-    lquat = tf.transformations.quaternion_from_euler(lpos[3], lpos[4], lpos[5])
-    lquat = tf.transformations.quaternion_from_euler(rpos[3], rpos[4], rpos[5])
-    #TODO: use Quaternion & coordinates to control Baxter
 
     while(len(pos) != 0):
         if(pos[0] != "Right hand"):
